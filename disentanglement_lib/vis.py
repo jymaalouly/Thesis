777a172bf579756fad2d7a90bc62732c7be177f7
jymaalouly <jymaalouly@gmail.com>
2020-12-01 01:05:38 +0000

    output

--- disentanglement_lib/vis.py
+++ disentanglement_lib/vis.py
@@ -1,4 +1,4 @@
 
 from disentanglement_lib.visualize import visualize_model
 
-visualize_model.visualize("/content/Thesis/disentanglement_lib/output/dsprites/vae/model","/content/Thesis/disentanglement_lib/gifs/dsprites",True)
+visualize_model.visualize("/content/Thesis/disentanglement_lib/output/dsprites/vae/model","/content/Thesis/disentanglement_lib/gifs/dsprites/vae",True)
