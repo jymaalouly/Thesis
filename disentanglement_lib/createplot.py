2a4129a1601547d0b25eb60630a3a9605a473f8b
jymaalouly <jymaalouly@gmail.com>
2020-12-30 19:35:29 +0000

    output

--- disentanglement_lib/createplot.py
+++ disentanglement_lib/createplot.py
@@ -5,11 +5,13 @@ import os
 import pandas as pd
 import numpy as np
 from matplotlib import cm 
+from sklearn import preprocessing
 
-mypath = os. getcwd()
+
+mypath = os.getcwd()
 output = "/content/Thesis/disentanglement_lib/data/"
 
-
+df1 = pd.read_csv("/content/Thesis/disentanglement_lib/csv/AER/CASchools.csv")
 counter = 0
 # number of data points
 num_data_points = 0
@@ -20,7 +22,7 @@ evenly_spaced_interval = np.linspace(0, 1, 5)
 colors = [cm.rainbow(x) for x in evenly_spaced_interval]
 graph_features = pd.DataFrame(columns=['pos' , 'size' , 'shape','color'])
 # draw the plot
-
+'''
 for f in range(0,25):
   num_data_points += 5
   print(f)
@@ -33,17 +35,30 @@ for f in range(0,25):
             for i, d in enumerate(colors):
               for c in range(len(marker)):
                     name = str(f) + '-pos-' + str(a) + '-size-' + str(b) + '-shape-' + str(c) + '-color-' + str(i) + '.png'
-                    '''plt.figure(figsize=(6,6))
+                    plt.figure(figsize=(6,6))
                     plt.scatter(x, y, s = b, marker = marker[c], c = np.array([d]))
                     plt.axis([0.0, 1.0, 0.0, 1.0])
                     plt.tight_layout()
                     plt.savefig( output + "scatt/" + name, dpi=10.7)
                     #plt.show()
-                    plt.close('all')'''
+                    plt.close('all')
                     graph_features = graph_features.append( {'pos':counter -1 , 'size':(int(b/500)-2) ,'shape':c, 'color' : i}, ignore_index=True)
 
 #print(graph_features)
 graph_features.to_csv(output + 'output.csv',index = False, header=False)
+'''
 
 
-
+for i in range(1,len(df1.columns)):
+  if df1.iloc[:, i].dtype.name == 'int64' or df1.iloc[:, i].dtype.name == 'float64':
+    x = preprocessing.normalize([df1.iloc[:, i]])
+    for t in range (i, len(df1.columns)):
+      if df1.iloc[:, t].dtype.name == 'int64' or df1.iloc[:, t].dtype.name == 'float64':
+        y = preprocessing.normalize([df1.iloc[:, t]])
+        plt.figure(figsize=(6,6))
+        plt.scatter(x, y)
+        plt.axis([0.0, 1.0, 0.0, 1.0])
+        plt.tight_layout()
+        plt.savefig( output + "scatt/" + str(i) + '-'+ str(t), dpi=10.7)
+        #plt.show()
+        plt.close('all')
