0581aaf031b56e0621cc3082765f97486d0e77a9
jymaalouly <jymaalouly@gmail.com>
2021-01-05 04:34:58 +0000

    output

--- disentanglement_lib/createplot.py
+++ disentanglement_lib/createplot.py
@@ -14,7 +14,7 @@ counter = 0
 # number of data points
 num_data_points = 0
 
-marker = ["o","s","v","p","H","X","D"]
+marker = ["o","s","p","H","X","D"]
 
 evenly_spaced_interval = np.linspace(0, 1, 5)
 colors = [cm.rainbow(x) for x in evenly_spaced_interval]
@@ -59,18 +59,18 @@ for index1, filename in enumerate(os.listdir('/content/Thesis/disentanglement_li
                 if df[column1].dtype.name == 'int64' or df[column1].dtype.name == 'float64':
                   y_scaled = min_max_scaler.fit_transform(df[[column1]])
                   y = pd.DataFrame(y_scaled)
-                  for b in range(200,1001):
-                    if (b % 200) == 0 :
+                  for b in range(50,301):
+                    if (b % 50) == 0 :
                         for i, d in enumerate(colors):
                           for c in range(len(marker)):
                                 name = str(index1)+ str(index2)+str(index3)+ '-size-' + str(b) + '-shape-' + str(c) + '-color-' + str(i) + '.png'
-                                '''plt.figure(figsize=(6,6))
+                                plt.figure(figsize=(6,6))
                                 plt.scatter(x, y, s = b, marker = marker[c], c = np.array([d]), alpha=0.7)
                                 plt.axis([0.0, 1.0, 0.0, 1.0])
                                 plt.tight_layout()
                                 plt.savefig( output + "scatt/" + name, dpi=10.7)
                                 #plt.show()
-                                plt.close('all')'''
+                                plt.close('all')
                                 graph_features = graph_features.append( {'size':(int(b/200)-1) ,'shape':c, 'color' : i}, ignore_index=True)
   except ValueError:
     print("Oops!  That was no valid number.  Try again...")
