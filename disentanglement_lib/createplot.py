7fbe7728f4ec72313bc0c127cccb499bff38fbd9
jymaalouly <jymaalouly@gmail.com>
2021-01-06 13:31:44 +0000

    output

--- disentanglement_lib/createplot.py
+++ disentanglement_lib/createplot.py
@@ -59,7 +59,7 @@ for index0, filename in enumerate(os.listdir('/content/Thesis/disentanglement_li
                 if df[column1].dtype.name == 'int64' or df[column1].dtype.name == 'float64':
                   y_scaled = min_max_scaler.fit_transform(df[[column1]])
                   y = pd.DataFrame(y_scaled)
-                  if not ((x == 0).all()) and not ((y == 0).all()) :
+                  if not ((x.iloc[0] == 0).all()) and not ((y.iloc[0] == 0).all()) :
                     name = str(index0)+ '-' + str(index1) + '-' + str(index2) + '-' + str(index3) + '.png'
                     plt.figure(figsize=(6,6))
                     plt.scatter(x, y, s = 50, marker = "o",alpha=0.7)
