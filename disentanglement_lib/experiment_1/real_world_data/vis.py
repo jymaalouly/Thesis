746346edacf76d008bfa2704fe5fc0980a9e0010
jymaalouly <jymaalouly@gmail.com>
2021-01-16 19:18:59 +0000

    output

---
+++ disentanglement_lib/experiment_1/real_world_data/vis.py
@@ -0,0 +1,4 @@
+
+from disentanglement_lib.visualize import visualize_model
+
+visualize_model.visualize("/content/Thesis/disentanglement_lib/experiment_1/synthetic_data/output/beta_vae_10000_latent4/model","/content/Thesis/disentanglement_lib/experiment_1/synthetic_data/beta_vae_10000_latent4",True)
