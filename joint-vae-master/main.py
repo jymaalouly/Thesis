889ac00c0f390f8f623784d0c54177cdbc194456
jymaalouly <jymaalouly@gmail.com>
2021-01-21 14:47:59 +0000

    output

---
+++ joint-vae-master/main.py
@@ -0,0 +1,40 @@
+import torch
+from jointvae.models import VAE
+from jointvae.training import Trainer
+from utils.dataloaders import get_sctterplot_dataloader
+from torch import optim
+
+
+batch_size = 64
+lr = 5e-4
+epochs = 50
+dataset_path = '/content/Thesis/disentanglement_lib/data/og/' 
+# Check for cuda
+use_cuda = torch.cuda.is_available()
+
+# Load data
+data_loader = get_sctterplot_dataloader(batch_size, dataset_path)
+img_size = (3, 64, 64)
+
+# Define latent spec and model
+latent_spec = {'cont': 32, 'disc': [0]}
+model = VAE(img_size=img_size, latent_spec=latent_spec,
+            use_cuda=use_cuda)
+if use_cuda:
+    model.cuda()
+
+# Define optimizer
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+# Define trainer
+trainer = Trainer(model, optimizer,
+                  cont_capacity=[0.0, 5.0, 25000, 30],
+                  disc_capacity=[0.0, 5.0, 25000, 30],
+                  use_cuda=use_cuda)
+
+# Train model for 100 epochs
+trainer.train(data_loader, epochs)
+
+# Save trained model
+torch.save(trainer.model.state_dict(), '/content/Thesis/joint-vae/trained_models/scatterplot/model.pt')
+asdasd
