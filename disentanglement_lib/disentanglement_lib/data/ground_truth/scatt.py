f5caed6fff4c5e8f11e02569b1ed3dd4cf84dc9f
jymaalouly <jymaalouly@gmail.com>
2021-02-12 15:20:03 +0000

    output

--- disentanglement_lib/disentanglement_lib/data/ground_truth/scatt.py
+++ disentanglement_lib/disentanglement_lib/data/ground_truth/scatt.py
@@ -27,7 +27,7 @@ import tensorflow.compat.v1 as tf
 from numpy import genfromtxt
 
 
-SCATT_PATH = os.path.join("/content/Thesis/disentanglement_lib/data")
+SCATT_PATH = os.path.join("/content/Thesis/disentanglement_lib/data/initial_data")
 
 
 
